a9676c422c1d6da935fe402c08d56cd54680b1e2
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2023-01-05 17:46:02 -0600

    solve day02 part 2

--- src/day02.rs
+++ src/day02.rs
@@ -20,26 +20,42 @@ pub mod day02 {
 
 
     pub fn rock_paper_scissors() {
-        let mut scores = HashMap::new();
-        scores.insert(String::from("A X"), TIE_POINTS+ROCK_POINTS);
-        scores.insert(String::from("B X"), LOSS_POINTS+ROCK_POINTS);
-        scores.insert(String::from("C X"), WIN_POINTS+ROCK_POINTS);
+        let mut scores_part1 = HashMap::new();
+        scores_part1.insert(String::from("A X"), TIE_POINTS+ROCK_POINTS);
+        scores_part1.insert(String::from("B X"), LOSS_POINTS+ROCK_POINTS);
+        scores_part1.insert(String::from("C X"), WIN_POINTS+ROCK_POINTS);
 
-        scores.insert(String::from("A Y"), WIN_POINTS+PAPER_POINTS);
-        scores.insert(String::from("B Y"), TIE_POINTS+PAPER_POINTS);
-        scores.insert(String::from("C Y"), LOSS_POINTS+PAPER_POINTS);
+        scores_part1.insert(String::from("A Y"), WIN_POINTS+PAPER_POINTS);
+        scores_part1.insert(String::from("B Y"), TIE_POINTS+PAPER_POINTS);
+        scores_part1.insert(String::from("C Y"), LOSS_POINTS+PAPER_POINTS);
 
-        scores.insert(String::from("A Z"), LOSS_POINTS+SCISSORS_POINTS);
-        scores.insert(String::from("B Z"), WIN_POINTS+SCISSORS_POINTS);
-        scores.insert(String::from("C Z"), TIE_POINTS+SCISSORS_POINTS);
+        scores_part1.insert(String::from("A Z"), LOSS_POINTS+SCISSORS_POINTS);
+        scores_part1.insert(String::from("B Z"), WIN_POINTS+SCISSORS_POINTS);
+        scores_part1.insert(String::from("C Z"), TIE_POINTS+SCISSORS_POINTS);
+
+        let mut scores_part2 = HashMap::new();
+        scores_part2.insert(String::from("A X"), LOSS_POINTS+SCISSORS_POINTS);
+        scores_part2.insert(String::from("B X"), LOSS_POINTS+ROCK_POINTS);
+        scores_part2.insert(String::from("C X"), LOSS_POINTS+PAPER_POINTS);
+
+        scores_part2.insert(String::from("A Y"), TIE_POINTS+ROCK_POINTS);
+        scores_part2.insert(String::from("B Y"), TIE_POINTS+PAPER_POINTS);
+        scores_part2.insert(String::from("C Y"), TIE_POINTS+SCISSORS_POINTS);
+
+        scores_part2.insert(String::from("A Z"), WIN_POINTS+PAPER_POINTS);
+        scores_part2.insert(String::from("B Z"), WIN_POINTS+SCISSORS_POINTS);
+        scores_part2.insert(String::from("C Z"), WIN_POINTS+ROCK_POINTS);
 
         let input = fs::read_to_string("inputs/input02.txt").unwrap();
 
-        let mut total = 0;
+        let mut total_part1 = 0;
+        let mut total_part2 = 0;
         for line in input.split("\n") {
-            total += scores.get(line).unwrap_or(&0);
+            total_part1 += scores_part1.get(line).unwrap_or(&0);
+            total_part2 += scores_part2.get(line).unwrap_or(&0);
         }
 
-        println!("Day 2 part 1 solution: total points = {total}");
+        println!("Day 2 part 1 solution: total points = {total_part1}");
+        println!("Day 2 part 2 solution: total points = {total_part2}");
     }
 }
